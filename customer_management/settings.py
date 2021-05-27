a3a46ff8f37191020d0ff51ed85d3a1c51302eb0
Prina Gudhka <prina.gudhka@somaiya.edu>
2021-05-27 23:10:22 +0530

    Update settings.py

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(5na=0%25(^+&1wq9nt)u@(561d#ya+9u7*fjshkgp#lk3#!#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
