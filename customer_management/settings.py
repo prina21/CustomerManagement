f310fcb4a59ec31c132e13bfb6710f206f157bab
prina <prina.gudhka@somaiya.edu>
2021-05-31 16:15:58 +0530

    settings changed

--- customer_management/settings.py
+++ customer_management/settings.py
@@ -73,6 +73,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'customer_management.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
